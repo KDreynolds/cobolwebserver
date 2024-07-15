27a165757eaf05d6fc2f2a82e24bbaa08aa7defd
Kyle Reynolds <kylereynoldsdev@gmail.com>
2024-07-14 22:13:04 -0600

    so damn close

--- init_server.c
+++ init_server.c
@@ -1,48 +1,162 @@
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdio.h>
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "Ws2_32.lib")
+#else
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <unistd.h>
+    #include <arpa/inet.h>
+    #define SOCKET int
+    #define INVALID_SOCKET -1
+    #define SOCKET_ERROR -1
+    #define closesocket close
+#endif
 
-#pragma comment(lib, "Ws2_32.lib")
+#include <stdio.h>
+#include <string.h>
 
 SOCKET init_server(int port) {
-    WSADATA wsaData;
-    SOCKET server_socket = INVALID_SOCKET;
+    SOCKET server_socket;
     struct sockaddr_in server_addr;
     
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
-        printf("WSAStartup failed\n");
-        return INVALID_SOCKET;
-    }
+    #ifdef _WIN32
+        WSADATA wsaData;
+        if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
+            printf("WSAStartup failed\n");
+            return INVALID_SOCKET;
+        }
+    #endif
 
-    // Create a socket
-    server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == INVALID_SOCKET) {
-        printf("Error creating socket: %d\n", WSAGetLastError());
-        WSACleanup();
+        printf("Error creating socket\n");
+        #ifdef _WIN32
+            WSACleanup();
+        #endif
         return INVALID_SOCKET;
     }
 
-    // Setup the server address structure
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
-    // Bind the socket
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        printf("Bind failed with error: %d\n", WSAGetLastError());
+        printf("Bind failed\n");
         closesocket(server_socket);
-        WSACleanup();
+        #ifdef _WIN32
+            WSACleanup();
+        #endif
         return INVALID_SOCKET;
     }
 
-    // Listen for incoming connections
     if (listen(server_socket, SOMAXCONN) == SOCKET_ERROR) {
-        printf("Listen failed with error: %d\n", WSAGetLastError());
+        printf("Listen failed\n");
         closesocket(server_socket);
-        WSACleanup();
+        #ifdef _WIN32
+            WSACleanup();
+        #endif
         return INVALID_SOCKET;
     }
 
+    printf("Server initialized on port %d\n", port);
     return server_socket;
+}
+
+SOCKET accept_connection(SOCKET server_socket) {
+    SOCKET client_socket;
+    struct sockaddr_in client_addr;
+    #ifdef _WIN32
+        int client_addr_len = sizeof(client_addr);
+    #else
+        socklen_t client_addr_len = sizeof(client_addr);
+    #endif
+
+    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_socket == INVALID_SOCKET) {
+        printf("Accept failed\n");
+        return INVALID_SOCKET;
+    }
+
+    printf("New connection accepted\n");
+    return client_socket;
+}
+void handle_connection(SOCKET client_socket) {
+    char buffer[1024] = {0};
+    char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from C Web Server!";
+
+    recv(client_socket, buffer, sizeof(buffer), 0);
+    printf("Received request:\n%s\n", buffer);
+
+    send(client_socket, response, strlen(response), 0);
+    printf("Response sent.\n");
+
+    closesocket(client_socket);
+}
+
+int read_request(SOCKET client_socket, char* buffer, int buffer_size) {
+    int bytes_received = recv(client_socket, buffer, buffer_size - 1, 0);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';  // Null-terminate the received data
+        return bytes_received;
+    } else if (bytes_received == 0) {
+        printf("Connection closed by client\n");
+    } else {
+        printf("recv failed\n");
+    }
+    return bytes_received;
+}
+
+int send_response(SOCKET client_socket, const char* response, int response_length) {
+    return send(client_socket, response, response_length, 0);
+}
+
+int main() {
+    SOCKET server_socket, client_socket;
+    int port = 8080;
+
+    server_socket = init_server(port);
+    if (server_socket == INVALID_SOCKET) {
+        printf("Server initialization failed\n");
+        return 1;
+    }
+
+    printf("Server is running on port %d\n", port);
+
+    while(1) {
+        client_socket = accept_connection(server_socket);
+        if (client_socket != INVALID_SOCKET) {
+            handle_connection(client_socket);
+        }
+    }
+
+    closesocket(server_socket);
+    #ifdef _WIN32
+        WSACleanup();
+    #endif
+
+    return 0;
+}
+
+
+void close_socket(SOCKET socket) {
+    closesocket(socket);
+}
+
+typedef SOCKET (*init_server_func)(int);
+typedef SOCKET (*accept_connection_func)(SOCKET);
+typedef int (*read_request_func)(SOCKET, char*, int);
+typedef int (*send_response_func)(SOCKET, const char*, int);
+typedef void (*close_socket_func)(SOCKET);
+
+void init_functions(init_server_func* init_server_ptr,
+                    accept_connection_func* accept_connection_ptr,
+                    read_request_func* read_request_ptr,
+                    send_response_func* send_response_ptr,
+                    close_socket_func* close_socket_ptr) {
+    *init_server_ptr = init_server;
+    *accept_connection_ptr = accept_connection;
+    *read_request_ptr = read_request;
+    *send_response_ptr = send_response;
+    *close_socket_ptr = close_socket;
 }
